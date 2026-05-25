f9761337e7483b91de347925a28547f1720bf6a7
redlotus <thedev1ops@gmail.com>
2026-05-25 21:58:25 +0530

    Update main.cpp

---
+++ 2026/24_05/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxJumps(vector<int>& arr, int d)
+    {
+        // Jump game problem solution goes here!
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    return 0;
+}
